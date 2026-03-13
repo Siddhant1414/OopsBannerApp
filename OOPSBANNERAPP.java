0c3405a54f58ef1f18676a4513c36749749093b4
siddhantkhatri <khatrisiddhant996@gmail.com>
2026-03-13 09:17:54 +0530

    UC6: Implement banner using static functions

--- OOPSBANNERAPP.java
+++ OOPSBANNERAPP.java
@@ -1,19 +1,49 @@
 public class OOPSBANNERAPP {
 
+    static String[] getO() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+        };
+    }
+
+    static String[] getP() {
+        return new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      ",
+            "*      "
+        };
+    }
+
+    static String[] getS() {
+        return new String[]{
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "      *",
+            "      *",
+            " ***** "
+        };
+    }
+
     public static void main(String[] args) {
 
-        String[] banner = {
-            String.join("", " ***** ", "  ", " ***** ", "  ", " ***** ", "  ", " ***** "),
-            String.join("", "*     *", "  ", "*     *", "  ", "*     *", "  ", "*     *"),
-            String.join("", "*     *", "  ", "*     *", "  ", "*     *", "  ", "*     *"),
-            String.join("", "*     *", "  ", "*     *", "  ", " ***** ", "  ", " ***** "),
-            String.join("", "*     *", "  ", "*     *", "  ", "*      ", "  ", "*      "),
-            String.join("", "*     *", "  ", "*     *", "  ", "*      ", "  ", "*      "),
-            String.join("", " ***** ", "  ", " ***** ", "  ", "*      ", "  ", "*      ")
-        };
+        String[] O = getO();
+        String[] P = getP();
+        String[] S = getS();
 
-        for (String line : banner) {
-            System.out.println(line);
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(O[i] + "  " + O[i] + "  " + P[i] + "  " + S[i]);
         }
     }
 }
